3e4858a2f62919ab12321f7c3e98b3820e93dc5f
memesmith0 <meme.smith.wizard@gmail.com>
2025-05-30 10:29:46 -0700

    Update jsonlambdas.py
    asodifjoaisdjfoaidsfj

--- jsonlambdas.py
+++ jsonlambdas.py
@@ -218,4 +218,14 @@
 #
 #The GNU General Public License does not permit incorporating your program into proprietary programs. If your program is a subroutine library, you may consider it more useful to permit linking proprietary applications with the library. If this is what you want to do, use the GNU Lesser General Public License instead of this License. But first, please read <https://www.gnu.org/licenses/why-not-lgpl.html>.
 
-#get input file
+#λx.x
+["lambda", "x", "x"]
+
+#λx.x x
+["lambda", "x", ["x", "x"]]
+
+#λx.λy.x
+["lambda", "x", ["lambda", "y", "x"]]
+
+#λx.λy.y
+["lambda", "x", ["lambda", "y", "y"]]
